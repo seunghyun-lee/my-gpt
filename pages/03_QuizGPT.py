e2d710069d4068a7d1c8c890dd07cc194e307bfa
Seunghyun Lee <seunghyunleekr@gmail.com>
2024-09-09 00:22:37 +0900

    add quiz gpt

--- pages/03_QuizGPT.py
+++ pages/03_QuizGPT.py
@@ -1,3 +1,300 @@
 import streamlit as st
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks import StreamingStdOutCallbackHandler
+from langchain.retrievers import WikipediaRetriever
+from langchain.schema import BaseOutputParser, output_parser
 
-st.title("Quiz GPT")
+class JsonOutputParser(BaseOutputParser):
+    def parse(self, text):
+        text = text.replace("```", "").replace("json", "").replace("true", "True").replace("false", "False")
+        return eval(text)
+
+output_parser = JsonOutputParser()
+
+st.set_page_config(page_title="QuizGPT", page_icon="❓")
+
+st.title("QuizGPT")
+
+llm = ChatOpenAI(
+    temperature=0.1,
+    model="gpt-3.5-turbo-1106",
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()],
+)
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+questions_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+    You are a helpful assistant that is role playing as a teacher.
+         
+    Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+    
+    Each question should have 4 answers, three of them must be incorrect and one should be correct.
+         
+    Use (o) to signal the correct answer.
+         
+    Question examples:
+         
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+         
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+         
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+         
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+         
+    Your turn!
+         
+    Context: {context}
+""",
+        )
+    ]
+)
+
+questions_chain = {"context": format_docs} | questions_prompt | llm
+
+formatting_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+    You are a powerful formatting algorithm.
+     
+    You format exam questions into JSON format.
+    Answers with (o) are the correct ones.
+     
+    Example Input:
+
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+         
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+         
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+         
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+    
+     
+    Example Output:
+     
+    ```json
+    {{ "questions": [
+            {{
+                "question": "What is the color of the ocean?",
+                "answers": [
+                        {{
+                            "answer": "Red",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Yellow",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Green",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Blue",
+                            "correct": true
+                        }},
+                ]
+            }},
+                        {{
+                "question": "What is the capital or Georgia?",
+                "answers": [
+                        {{
+                            "answer": "Baku",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Tbilisi",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Manila",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Beirut",
+                            "correct": false
+                        }},
+                ]
+            }},
+                        {{
+                "question": "When was Avatar released?",
+                "answers": [
+                        {{
+                            "answer": "2007",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2001",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2009",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "1998",
+                            "correct": false
+                        }},
+                ]
+            }},
+            {{
+                "question": "Who was Julius Caesar?",
+                "answers": [
+                        {{
+                            "answer": "A Roman Emperor",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Painter",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Actor",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Model",
+                            "correct": false
+                        }},
+                ]
+            }}
+        ]
+     }}
+    ```
+    Your turn!
+
+    Questions: {context}
+
+""",
+        )
+    ]
+)
+
+formatting_chain = formatting_prompt | llm
+
+@st.cache_data(show_spinner="Loading file...")
+def split_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/quiz_files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    return docs
+
+@st.cache_data(show_spinner="Making quiz...")
+def run_quiz_chain(_docs, topic):
+    chain = {"context": questions_chain} | formatting_chain | output_parser
+    return chain.invoke(_docs)
+
+@st.cache_data(show_spinner="Searching Wikipedia...")
+def wiki_search(term):
+    retriever = WikipediaRetriever(top_k_result=5)
+    docs = retriever.get_relevant_documents(term)
+    return docs
+
+with st.sidebar:
+    docs = None
+    choice = st.selectbox(
+        "Choose what you want to use.",
+        (
+            "File",
+            "Wikipedia Article",
+        ),
+    )
+    if choice == "File":
+        file = st.file_uploader(
+            "Upload a .docx , .txt or .pdf file",
+            type=["pdf", "txt", "docx"],
+        )
+        if file:
+            docs = split_file(file)
+            st.session_state.quiz_data = None  # 파일 변경 시 이전 퀴즈 데이터 삭제
+            st.session_state.submitted = False
+    else:
+        topic = st.text_input("Search Wikipedia...", key="topic_input")
+        if topic:
+            # 이전 검색어와 새로운 검색어가 다르면 세션 상태 초기화
+            if "last_topic" in st.session_state and st.session_state["last_topic"] != topic:
+                st.session_state.quiz_data = None
+                st.session_state.submitted = False
+            st.session_state["last_topic"] = topic
+            docs = wiki_search(topic)
+
+if not docs:
+    st.markdown(
+        """
+    Welcome to QuizGPT.
+                
+    I will make a quiz from Wikipedia articles or files you upload to test your knowledge and help you study.
+                
+    Get started by uploading a file or searching on Wikipedia in the sidebar.
+    """
+    )
+else:
+    if "quiz_data" not in st.session_state or st.session_state.quiz_data is None:
+        start = st.button("Generate Quiz")
+
+        if start:
+            response = run_quiz_chain(docs, topic if topic else file.name)
+            st.session_state.quiz_data = response
+            st.session_state.quiz_started = True
+            st.session_state.submitted = False
+    else:
+        st.session_state.quiz_started = True
+
+    if st.session_state.get("quiz_started") and st.session_state.quiz_data:
+        if "questions" in st.session_state.quiz_data:
+            with st.form("questions_form"):
+                for idx, question in enumerate(st.session_state.quiz_data["questions"]):
+                    st.write(f"{idx+1}. " + question["question"])
+                    value = st.radio(
+                        "Select an option.",
+                        [answer["answer"] for answer in question["answers"]],
+                        key=f"{idx}_radio",
+                        index=None,
+                    )
+                    st.session_state[f"q{idx}_answer"] = value
+
+                    # 정답 표시: 제출 버튼 누른 후에만 표시
+                    if st.session_state.get("submitted"):
+                        selected_answer = st.session_state.get(f"q{idx}_answer")
+                        correct_answer = next(
+                            (answer["answer"] for answer in question["answers"] if answer["correct"]), None
+                        )
+                        if selected_answer == correct_answer:
+                            st.success("Correct!")
+                        else:
+                            st.error(f"Wrong! The correct answer was {correct_answer}.")
+
+                submit_button = st.form_submit_button("Submit")
+
+            if submit_button:
+                st.session_state.submitted = True
+        else:
+            st.error("No questions found in the quiz data.")
