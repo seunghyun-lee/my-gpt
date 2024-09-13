eac8497e40583ca4feed54e1635941b245674dbc
Seunghyun Lee <seunghyunleekr@gmail.com>
2024-09-14 01:03:46 +0900

    Whisper Transcript

--- Home.py
+++ Home.py
@@ -1,5 +1,30 @@
 import streamlit as st
 
+def hide_sidebar():
+    st.set_page_config(initial_sidebar_state="collapsed")
+    st.markdown(
+        """
+        <style>
+        [data-testid="collapsedControl"] {display: none}
+        #MainMenu {visibility: hidden;}
+        .css-zt5igj {display: none;}
+        </style>
+    """,
+        unsafe_allow_html=True,
+    )
+
+def show_sidebar():
+    st.markdown(
+        """
+        <style>
+        [data-testid="collapsedControl"] {display: flex}
+        #MainMenu {visibility: visible;}
+        .css-zt5igj {display: block;}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
 st.set_page_config(
     page_title="FullstackGPT Home",
     page_icon="🤖",
