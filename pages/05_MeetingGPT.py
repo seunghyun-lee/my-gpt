6e09fd8efe5f128e1bbe11b8b6e50259cf0ebe1c
Seunghyun Lee <seunghyunleekr@gmail.com>
2024-09-14 15:21:36 +0900

    meeting gpt

--- pages/05_MeetingGPT.py
+++ pages/05_MeetingGPT.py
@@ -1,3 +1,190 @@
 import streamlit as st
+import subprocess
+from pydub import AudioSegment
+import math
+import glob
+import openai
+import os
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import StrOutputParser
+from langchain.vectorstores.faiss import FAISS
+from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.storage import LocalFileStore
 
-st.title("Meeting GPT")
+llm = ChatOpenAI(
+    temperature=0.1, model="gpt-4o-mini"
+)
+
+has_transcript = os.path.exists("./.cache/videoplayback.txt")
+
+@st.cache_resource()
+def embed_file(file_path):
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=800,
+        chunk_overlap=100,
+    )
+    loader = TextLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+@st.cache_data()
+def extract_audio_from_video(video_path):
+    if has_transcript:
+        return
+    audio_path = video_path.replace("mp4", "mp3")
+    command = [
+        "ffmpeg", 
+        "-i",
+        video_path,
+        "-vn",
+        audio_path,
+        ]
+    subprocess.run(command)
+
+@st.cache_data()
+def cut_audio_in_chunks(audio_path, chunk_size, chunks_folder):
+    if has_transcript: 
+        return
+    track = AudioSegment.from_mp3(audio_path)
+    chunk_len = chunk_size * 60 * 1000
+    chunks = math.ceil(len(track) / chunk_len)
+    for i in range(chunks):
+        start_time = i * chunk_len
+        end_time = (i + 1) * chunk_len
+        chunk = track[start_time:end_time]
+        chunk.export(
+            f"./{chunks_folder}/chunk_{str(i).zfill(2)}.mp3", 
+            format="mp3"
+            )
+
+@st.cache_data()
+def transcribe_chunks(chunks_folder, destination):
+    if has_transcript:
+        return
+    files = glob.glob(f"{chunks_folder}/*.mp3")
+    files.sort()
+    for file in files:
+        with open(file, "rb") as audio_file, open(destination, "a") as text_file:
+            transcript = openai.audio.transcriptions.create(
+                model="whisper-1",
+                file=audio_file,
+                language="ko"
+            )
+            text_file.write(transcript.text)
+
+st.set_page_config(
+    page_title="MeetingGPT",
+    page_icon="💼"
+)
+
+st.markdown(
+    """
+# MeetingGPT
+            
+Welcome to MeetingGPT, upload a video and I will give you a transcript, a summary and a chat bot to ask any questions about it.
+
+Get started by uploading a video file in the sidebar.
+"""
+)
+
+with st.sidebar:
+    video = st.file_uploader(
+        "Video",
+        type=["mp4","avi","mkv","mov"],
+    )
+
+if video:
+    chunk_folder = "./.cache/chunks"
+    with st.status("Loading video...") as status:
+        video_content = video.read()
+        video_path = f"./.cache/{video.name}"
+        audio_path = video_path.replace("mp4", "mp3")
+        transcript_path = video_path.replace("mp4", "txt")
+        with open(video_path, "wb") as f:
+            f.write(video_content)
+        status.update(label="Extracting audio...")
+        extract_audio_from_video(video_path)
+        status.update(label="Cutting audio segments...")
+        cut_audio_in_chunks(audio_path, 10, chunk_folder)
+        status.update(label="Transcribing audio...")
+        transcribe_chunks(chunk_folder, transcript_path)
+    
+    transcript_tab, summary_tab, qa_tab = st.tabs(
+        [
+            "Transcript",
+            "Summary",
+            "Q&A",
+        ]
+    )
+
+    with transcript_tab:
+        with open(transcript_path, "r") as file:
+            st.write(file.read())
+
+    with summary_tab:
+        start = st.button("Generate summary")
+
+        if start:
+            loader = TextLoader(transcript_path)
+            splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+                chunk_size=800,
+                chunk_overlap=100,
+            )
+            docs = loader.load_and_split(text_splitter=splitter)
+
+            first_summary_prompt = ChatPromptTemplate.from_template(
+                """
+                다음의 내용을 간결한 요악으로 작성해 주세요:
+                "{text}"
+                간결한 요악:
+            """
+            )
+
+            first_summary_chain = first_summary_prompt | llm | StrOutputParser()
+
+            summary = first_summary_chain.invoke(
+                {"text": docs[0].page_content},
+            )
+            
+            refine_prompt = ChatPromptTemplate.from_template(
+                """
+                최종 요약본을 작성하는 것이 업무입니다.
+                특정 시점까지 기존 요약을 제공했습니다: {existing_summary}
+                아래에서 더 많은 맥락으로 기존 요약을 개선할 수 있는 기회가 있습니다(필요한 경우에만).
+                ------------
+                {context}
+                ------------
+                새로운 컨텍스트가 주어지면 원래 요약을 다듬습니다.
+                컨텍스트가 유용하지 않은 경우 원래 요약을 반환합니다.
+            """
+            )
+            
+            refine_chain = refine_prompt | llm | StrOutputParser()
+
+            with st.status("Summarizing...") as status:
+                for i, doc in enumerate(docs[1:]):
+                    status.update(label=f"Processing document {i+1}/{len(docs)-1} ")
+                    summary = refine_chain.invoke(
+                        {
+                            "existing_summary": summary,
+                            "context": doc.page_content,
+                        }
+                    )
+                    st.write(summary)
+            st.write(summary)
+
+    with qa_tab:
+        qna = st.text_input("Write a question about the summary")
+        if qna:
+            retriever = embed_file(transcript_path)
+            docs = retriever.invoke(qna)
+
+            st.write(docs)
